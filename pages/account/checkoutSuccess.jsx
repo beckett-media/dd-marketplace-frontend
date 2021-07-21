8beb5159f89795b971b1b8c69f15955bc42f0604
dev7thpillar <dev1@7thpillar.com>
2021-07-21 18:45:33 +0530

    browser icon changed

--- pages/account/checkoutSuccess.jsx
+++ pages/account/checkoutSuccess.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Result, Button, Row } from 'antd';
+import ContainerPage from '~/components/layouts/ContainerPage';
 
 const CheckoutSuccess = () => {
     return (
-        <Row align="middle" justify="center" style={{ height: '100vh' }}>
-            <Result
-                status="success"
-                title="Thank you"
-                subTitle="Your order has been confirmed"
-                extra={[
-                    <Button
-                        href="/account/invoices"
-                        type="primary"
-                        key="console">
-                        View Orders
-                    </Button>,
-                    <Button href="/shop" key="buy">
-                        Continue Shopping
-                    </Button>,
-                ]}
-            />
-        </Row>
+        <ContainerPage boxed={true} title="Address">
+            <Row align="middle" justify="center" style={{}}>
+                <Result
+                    status="success"
+                    title="Thank you"
+                    subTitle="Your order has been confirmed"
+                    extra={[
+                        <Button
+                            href="/account/invoices"
+                            type="primary"
+                            key="console">
+                            View Orders
+                        </Button>,
+                        <Button href="/shop" key="buy">
+                            Continue Shopping
+                        </Button>,
+                    ]}
+                />
+            </Row>
+        </ContainerPage>
     );
 };
 
