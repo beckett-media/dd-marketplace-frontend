33dc5f278be0e41cb1091d27ad89b17b089a9199
7thPillar-Ritto <ritto@7thpillar.com>
2021-08-21 08:53:56 +0530

    Fixed OS Type

--- repositories/utils.js
+++ repositories/utils.js
@@ -1,8 +1,9 @@
-export function getOsType() {
+export function getOS() {
     var os = 'unknown_os';
-    if (navigator.userAgent.indexOf('Win') != -1) os = 'windows';
-    if (navigator.userAgent.indexOf('Mac') != -1) os = 'mac_os';
-    if (navigator.userAgent.indexOf('Linux') != -1) os = 'linux';
-
+    if (navigator.userAgent.indexOf('Win') !== -1) os = 'windows_os';
+    if (navigator.userAgent.indexOf('Mac') !== -1) os = 'mac_os';
+    if (navigator.userAgent.indexOf('Linux') !== -1) os = 'linux_os';
+    if (navigator.userAgent.indexOf('android') !== -1) os = 'android';
+    if (navigator.userAgent.indexOf('ios') !== -1) os = 'ios';
     return os;
 }
