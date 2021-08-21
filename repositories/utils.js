8858b7887e5f17c7480707b8614ecb55c9596ceb
7thPillar-Ritto <ritto@7thpillar.com>
2021-08-21 09:02:06 +0530

    Updated Function Name

--- repositories/utils.js
+++ repositories/utils.js
@@ -1,4 +1,4 @@
-export function getOS() {
+export function getOsType() {
     var os = 'unknown_os';
     if (navigator.userAgent.indexOf('Win') !== -1) os = 'windows';
     if (navigator.userAgent.indexOf('Mac') !== -1) os = 'mac_os';
