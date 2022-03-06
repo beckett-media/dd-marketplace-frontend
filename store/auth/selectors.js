ae04c0967e2dcb8ecceeabb4db69b0a3399557b3
Muhammad Waqas Ilyas <qaskhan02@gmail.com>
2022-03-06 21:02:43 +0500

    fix issue user stripe id

--- store/auth/selectors.js
+++ store/auth/selectors.js
@@ -4,4 +4,4 @@ export const isUserAuthenticated = (state) => getData(state).isLoggedIn;
 
 export const getUserInfo = (state) => getData(state).user;
 
-export const getUserStripeId = (state) => getUserInfo(state)?.stripeUserId;
+export const getUserStripeId = (state) => getUserInfo(state)?.stripeId;
